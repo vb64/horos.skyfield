ec309384e6d7ad86090039afb5b853a62ab9a0d7
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-06-03 21:07:34 +0300

    step

--- src/zodiac.py
+++ src/zodiac.py
@@ -1,13 +1,36 @@
-class Zodiac:
-    Aries = 0
-    Taurus = 0
-    Gemini = 0
-    Cancer = 0
-    Leo = 0
-    Virgo = 0
-    Libra = 0
-    Scorpio = 0
-    Sagittarius = 0
-    Capricorn = 0
-    Aquarius = 0
-    Pisces = 0
+import math
+
+
+names = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces",
+]
+
+
+def borders(ecliptic_longtitude_radian):
+    return []
+
+
+def borders_degrees(ecliptic_longtitude_degrees):
+    """
+    >>> borders_degrees(10)
+    ["Pisces", "Aries"]
+    >>> borders_degrees(20)
+    ["Aries", "Taurus"]
+    """
+    return borders(math.radians(ecliptic_longtitude_degrees))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
